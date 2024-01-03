a62ecd26a0773499dcbc09d84a4f22392f0a61b8
jolaade <fayinminnuibijolaade@gmail.com>
2024-01-03 22:09:32 +0100

    code update

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 const firstName = document.getElementById("name")
 const greetingCard = document.getElementById("greeting")
+const inputField = document.getElementById("inputField")
 
-function save(){
-    
+function submit(){
+
     const save = localStorage.setItem( "name", firstName.value )
 
-}
-
-function getCard(){
-
     const name = localStorage.getItem("name")
-    let message = "Hello " + name + " , welcome to a fresh episode of today's activities"
-    greetingCard.innerHTML = message;
+    
+    let message = "Hello " + name + " !. Welcome aboard. It's good to have you here"
 
-    if(name == null){
-        greetingCard.innerHTML = "Please enter your name and save to get your card"
-    }
+    greetingCard.innerHTML = message;
+    
+    inputField.style.display = "none";
+    document.getElementById("submit").style.display = "none"
 
 }
+
+// function getCard(){
+
+//     const name = localStorage.getItem("name")
+//     let message = "Hello " + name + " , welcome to a fresh episode of today's activities"
+//     greetingCard.innerHTML = message;
+
+//     if(name == null){
+//         greetingCard.innerHTML = "Please enter your name and save to get your card"
+//     }
+
+// }
 
 function clearData(){
     const card = localStorage.clear()
     firstName.value = ""
     greetingCard.innerHTML = ""
+    inputField.style.display = "block"
+    document.getElementById("submit").style.display = "block"
+
 }
