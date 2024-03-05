6c4351cbd6b9f692f548af4ea77efdd6f614d0a1
jolaade <fayinminnuibijolaade@gmail.com>
2024-03-05 11:26:19 +0100

    a redo

--- script.js
+++ script.js
@@ -4,30 +4,17 @@ const inputField = document.getElementById("inputField")
 
 function submit(){
 
-    const save = localStorage.setItem( "name", firstName.value )
+    const save = localStorage.setItem( "name", firstName.value)
 
-    const name = localStorage.getItem("name")
+    const name = localStorage.getItem("name");
+     
+     
+    let message = "Hello " + name + " !. Welcome aboard. It's good to have you here" 
     
-    let message = "Hello " + name + " !. Welcome aboard. It's good to have you here"
-
     greetingCard.innerHTML = message;
-    
-    inputField.style.display = "none";
-    document.getElementById("submit").style.display = "none"
 
 }
-
-// function getCard(){
-
-//     const name = localStorage.getItem("name")
-//     let message = "Hello " + name + " , welcome to a fresh episode of today's activities"
-//     greetingCard.innerHTML = message;
-
-//     if(name == null){
-//         greetingCard.innerHTML = "Please enter your name and save to get your card"
-//     }
-
-// }
+console.log(submit)
 
 function clearData(){
     const card = localStorage.clear()
